7f3acf8ef1ba38be7eaaed509986a1c250277311
gal <gala@hackeruso.com>
2021-08-20 16:35:08 +0300

    feat(content types)

---
+++ app/Domain/General/Models/LuContentType.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Domain\General\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class LuContentType extends Model
+{
+
+}
--- app/Domain/Orders/Models/Order.php
+++ app/Domain/Orders/Models/Order.php
@@ -2,6 +2,7 @@
 
 namespace App\Domain\Tags\Models;
 
+use App\Domain\General\Models\LuContentType;
 use App\Domain\Tags\Models\OrderLog;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,5 +11,10 @@ class Order extends Model
     public function logs()
     {
         return $this->hasOne(OrderLog::class, 'order_id', 'id');
+    }
+    
+    public function contentType()
+    {
+        return $this->hasOne(LuContentType::class, 'id', 'content_type_id');
     }
 }
