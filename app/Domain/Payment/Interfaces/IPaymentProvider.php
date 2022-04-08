d4dafa762a3fc8bce1cc28a0d1639e9649e367ad
galbalak <gal.blacky@gmail.com>
2022-04-08 16:31:18 +0300

    feat(update the provider interface)

--- app/Domain/Payment/Interfaces/IPaymentProvider.php
+++ app/Domain/Payment/Interfaces/IPaymentProvider.php
@@ -8,7 +8,7 @@ interface IPaymentProvider
 {
     public function getProviderID(): int;
     public function setPrice(int $price);
-    public function setQuantity(int $quantity = 1);
+    public function setQuantity(int $quantity);
     public function setCurrency(string $currency);
     public function buildPayment(Order $order);
     public function pay();
