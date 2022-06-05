0601443ceda49874938f00ac6773c28ad4d6e433
Gal Albalak <gala@hackeruso.com>
2022-06-05 17:30:29 +0300

    Feat(new course email template)

--- resources/views/mails/user/newCourse.blade.php
+++ resources/views/mails/user/newCourse.blade.php
@@ -3,51 +3,117 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>New Course</title>
+    <title>MiToo - Forgot Password</title>
+
+    <style>
+        body {
+            direction: rtl;
+            font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+            padding: 0;
+            margin: 0;
+        }
+
+        table {
+            width: 40%;
+            margin: auto;
+        }
+
+        .content {
+            position: relative;
+            top: -200px;
+            background-color: #fff;
+            width: 94%;
+            border-radius: 10px;
+            box-shadow: 0 0 10px 2px #8885;
+        }
+
+        .content div {
+            padding: 1px 10px;
+            padding-bottom: 10px;
+            text-align: right;
+        }
+
+        .content img {
+            width: 60%;
+        }
+
+        .blue--text {
+            color: #16588f;
+        }
+
+        button {
+            background-color: #e6b260;
+            padding: 10px 30px;
+            border-radius: 25px;
+            border: 1px solid #fff;
+            box-shadow: 0 0 5px 1px #e6b26088;
+            color: #fff;
+            font-weight: bold;
+            cursor: pointer;
+        }
+
+        .cancel--button {
+            background-color: #333;
+            box-shadow: 0 0 5px 1px #3338;
+        }
+
+        a {
+            text-decoration: none;
+        }
+
+        @media only screen and (max-width: 600px) {
+            table {
+                width: 100%;
+                border-spacing: unset;
+            }
+
+            .header {
+                border-radius: unset;
+            }
+        }
+
+    </style>
 </head>
 <body>
     <table>
-        <thead>
-            <tr>
-                <td align="center">
-                    <h1>
-                    New Course
-                    </h1>
-                </td>
-            </tr>
-        </thead>
+        @include('mails.templates.header')
         <tbody>
             <tr>
                 <td align="center">
-                    <p>
-                        Hello {{ $data->user_name }},
+                    <div class="content">
+                        <div>
+                            <h1>
+                                היי {{ $data['name'] }}, <span class="blue--text">בהצלחה בקורס!</span>
+                            </h1>
+                            <p>
+                                הרגע התחלת את הקורס "{{ $data['course_name'] }}".
+                                <br>
+                                יש לך גישה עכשיו לכל תכני הקורס עד התאריך "{{ $data['end_at'] }}".
+                                <br>
+                                בשביל להיכנס לקורס לחץ על הכפתור למטה.
+                            </p>
+                        </div>
                         <br>
-                        You have just started the course "{{ $data->course }}".
+                        <img src="{{ URL::to('/') }}/files/general/dark-logo.png" />
                         <br>
-                        The course cost {{ $data->price }} NIS.
                         <br>
-                        The course ends at {{ $data->end_at }}.
                         <br>
-                        Thanks
-                    </p>
+                        <br>
+
+                        <a href="{{URL::to('/')}}/courses/{{ $data['course_id'] }}">
+                            <button>
+                                היכנס לקורס
+                            </button>
+                        </a>
+
+                        <br>
+                        <br>
+                        <br>
+                        <br>
+                    </div>
                 </td>
             </tr>
-            <tr>
-                <td align="left">
-                    <a href="{{URL::to('/')}}/delete-account?email={{ $data->email }}&token={{ $data->token }}&status=1">
-                        <button>
-                            Confirm
-                        </button>
-                    </a>
-                </td>
-                <td align="right">
-                    <a href="{{URL::to('/')}}/delete-account?email={{ $data->email }}&token={{ $data->token }}&status=0">
-                        <button>
-                            Cancel Request
-                        </button>
-                    </a>
-                </td>
-            </tr>
+            @include('mails.templates.footer')
         </tbody>
     </table>
 </body>
--- routes/groups/tests/mails.php
+++ routes/groups/tests/mails.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/email-confirmation', function() {
@@ -32,4 +33,16 @@ Route::get('/delete-account-request', function() {
         'token' => 'ASD3FV32f233fdfsadfdsf'
     ];
     return view('mails.profile.deleteAccountRequest', ['data' => $data]);
+});
+
+Route::get('/new-course', function() {
+    $date = new Carbon;
+    $data = [
+        'name'          => 'עדן',
+        'course_name'   => 'כדורגל בסיסי',
+        'course_id'     => 1,
+        'end_at'        => $date->addDays(364)->format('d/m/Y'),
+        'token'         => 'ASD3FV32f233fdfsadfdsf',
+    ];
+    return view('mails.user.newCourse', ['data' => $data]);
 });
