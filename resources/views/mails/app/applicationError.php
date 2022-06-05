140e47085f9631a6f6c12fca3f631048d40347de
Gal Albalak <gala@hackeruso.com>
2022-06-05 18:22:03 +0300

    fix(all templates titles)

--- resources/views/mails/app/applicationError.php
+++ resources/views/mails/app/applicationError.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MiToo - Application Error</title>
+    <title>Goldens -Application Error</title>
 </head>
 <body>
     <table>
--- resources/views/mails/profile/deleteAccountRequest.blade.php
+++ resources/views/mails/profile/deleteAccountRequest.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MiToo - Forgot Password</title>
+    <title>Goldens - Delete Account Request</title>
 
     <style>
         body {
