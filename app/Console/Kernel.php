3bed9205888c50df4192972f01960bfb5dcaf2d0
Chief <gala@hackeruso.com>
2022-08-06 15:28:18 +0300

    feat(course expired cron job)

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,7 +2,9 @@
 
 namespace App\Console;
 
+use App\Domain\Content\Services\ContentService;
 use App\Domain\Helpers\LogService;
+use App\Domain\Helpers\MailService;
 use App\Domain\Users\Services\UserService;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
@@ -21,6 +23,8 @@ class Kernel extends ConsoleKernel
 
     private LogService  $log_service;
 
+    private Schedule $schedule;
+
     /**
      * Define the application's command schedule.
      *
@@ -30,16 +34,23 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $this->log_service  = new LogService('scheduler');
+        $this->schedule     = $schedule;
 
+        
         $this->checkForExpiredCourses();
     }
 
+    /**
+     * Runs once a day at 00:00
+    */
     private function checkForExpiredCourses()
     {
         try {
-            $service = new DisableExpiredUserCoursesService;
-            $service->handler();
-            $this->log_service->info('checkForExpiredCourses ran successfully');            
+            $this->schedule->call(function () {
+                $service = new DisableExpiredUserCoursesService(new ContentService, new MailService);
+                $service->handler();
+                $this->log_service->info('checkForExpiredCourses ran successfully');            
+            })->cron('0 0 * * *');
         } catch (\Exception $ex) {
             $this->log_service->error($ex);
         }
