505fdd9907f850221213560c11d658e8b7d6423a
Blacky <gal.blacky@gmail.com>
2023-05-23 16:43:34 +0300

    feat(change videos to cloudfront)

--- app/Domain/Content/Models/Video.php
+++ app/Domain/Content/Models/Video.php
@@ -18,7 +18,7 @@ class Video extends Model
 
     public function getVideoSrcAttribute()
     {
-        return config('content.videos') . '/' . $this->video_path;  
+        return config('content.videos') . '/' . str_replace('content/videos/', '', $this->video_path);  
     }   
 
     public function lessons()
