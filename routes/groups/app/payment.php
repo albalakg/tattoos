e4c002b73ae28b0a12f1c2c1c43c0f0b3948e491
Blacky <gal.blacky@gmail.com>
2023-07-04 17:37:25 +0300

    feat(test payments)

--- routes/groups/app/payment.php
+++ routes/groups/app/payment.php
@@ -4,3 +4,4 @@ use App\Domain\Orders\Controllers\OrderController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('callback',  [OrderController::class, 'callback']);
+Route::post('callback',  [OrderController::class, 'callback']);
